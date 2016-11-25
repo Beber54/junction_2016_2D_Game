d738f1ddd1f2f93f63777ff73fc7e120488b0568
houdayec <houdayer.corentin@gmail.com>
2016-11-26 01:08:58 +0200

    installation of basic stuff to begin (state management)

---
+++ core/src/com/mygdx/game/States/MenuState.java
@@ -0,0 +1,29 @@
+package com.mygdx.game.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Corentin on 26/11/2016.
+ */
+
+public class MenuState extends State {
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void bundleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+}
